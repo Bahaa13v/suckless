3031469b7f13a4b5dfd23dc52eae8f66833940bf
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-10 10:38:59 +0100

    Update suckless.sh

--- suckless.sh
+++ suckless.sh
@@ -4,8 +4,10 @@ cd dwm
 sudo make clean install
 cd ../st
 sudo make clean install
-cd../dmenu
+cd ../dmenu
 sudo make clean install
-cd../surf
+cd ../surf
 sudo make clean install
+cd ..
 rm README.md
+rm xinitrc
