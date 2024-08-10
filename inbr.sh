b48b6c992febde7adc9e1640482e1f51349667ce
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-10 15:18:25 +0100

    Add files via upload

---
+++ inbr.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+current_brightness=$(xrandr --verbose | grep -i brightness | awk '{print $2}')
+
+new_brightness=$(echo "$current_brightness + 0.1" | bc)
+
+if (( $(echo "$new_brightness > 1.0" | bc -l) )); then
+    new_brightness=1.0
+fi
+
+xrandr --output LVDS1 --brightness $new_brightness
