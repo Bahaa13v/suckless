71b5b7bc272b34ea37861a59c3c7295b9502c706
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-10 10:38:02 +0100

    Update suckless.sh

--- suckless.sh
+++ suckless.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 cp -r xinitrc /home/bahaa/.xinitrc
+cd dwm
+sudo make clean install
+cd ../st
+sudo make clean install
+cd../dmenu
+sudo make clean install
+cd../surf
+sudo make clean install
+rm README.md
