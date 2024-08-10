4e5a6af8dafd8f038e316e8c7c5ed1f7846b29fb
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-10 13:51:28 +0100

    Update suckless.sh

--- suckless.sh
+++ suckless.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+chmod +x alias.sh
+mkdir -p /home/bahaa/scripts
+cp -r alias.sh /home/bahaa/scripts/
 cp -r xinitrc /home/bahaa/.xinitrc
 cd dwm
 sudo make clean install
@@ -11,3 +14,4 @@ sudo make clean install
 cd ..
 rm README.md
 rm xinitrc
+rm alias.sh
