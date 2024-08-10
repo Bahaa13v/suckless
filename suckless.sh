26662cf20b638d71b8eb504fb9dc51936372bef5
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-10 15:20:50 +0100

    Update suckless.sh

--- suckless.sh
+++ suckless.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+
 chmod +x alias.sh
+chmod +x inbr.sh
+chmod +x dcbr.sh
+
 mkdir -p /home/bahaa/scripts
+
 cp -r alias.sh /home/bahaa/scripts/
+cp -r inbr.sh /home/bahaa/scripts/
+cp -r dcbr.sh /home/bahaa/scripts/
+
 cp -r xinitrc /home/bahaa/.xinitrc
+
 cd dwm
 sudo make clean install
 cd ../st
@@ -12,6 +21,9 @@ sudo make clean install
 cd ../surf
 sudo make clean install
 cd ..
+
 rm README.md
 rm xinitrc
 rm alias.sh
+rm inbr.sh
+rm dcbr.sh
