8b7286733fe768622b01ad0e4ddec9a1b6a5b83c
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-13 20:13:36 +0100

    Update suckless.sh

--- suckless.sh
+++ suckless.sh
@@ -7,11 +7,11 @@ chmod +x inbr.sh
 chmod +x dcbr.sh
 
 mkdir -p /home/bahaa/scripts
-
+mkdir -p /home/bahaa/.confing/compton/
 cp -r alias.sh /home/bahaa/scripts/
 cp -r inbr.sh /home/bahaa/scripts/
 cp -r dcbr.sh /home/bahaa/scripts/
-
+cp -r compton.conf /home/bahaa/confing/compton/
 cp -r xinitrc /home/bahaa/.xinitrc
 echo "alias x=startx" >> /home/bahaa/.zshrc
 
